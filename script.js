2df6301d03cf8302a0aa73532b361cb8b9daea89
coderShreyash <69112238+coderShreyash@users.noreply.github.com>
2020-11-17 17:33:02 +0530

    Testing
    Results to be declared soon

---
+++ script.js
@@ -0,0 +1,7 @@
+var database=firebase.database();
+function extraf(){
+    document.getElementById("i1").setAttribute('src',"extra.html");
+}
+function blogf(){
+    document.getElementById("i1").setAttribute('src',"blog.html");
+}
